994f0879249b148932ace881f08697455a198bd4
summerlovej1 <30863086+summerlovej1@users.noreply.github.com>
2017-09-28 17:42:02 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,13 +21,25 @@ function initCircles() {
 }
 // draw circles with a given color
 function drawCircles() {
-	var size = random(10,20);
+	var size1 = 12;
+	var size2 = 17;
     fill(255,255,255);
     noStroke();
 
     for (i = 0; i < N; i++) {
-        cx[i] = cx[i] + -0.3;
-        cy[i] = cy[i] + 1;
-        ellipse(cx[i], cy[i], size, size);
+    	cx[i] = cx[i] + 0.1;
+        cy[i] = cy[i] + 1.5;
+        if(i%2 == 0){
+        	cy[i] = cy[i]+ 0.8;
+        	ellipse(cx[i], cy[i], size2, size2);
+        }
+        ellipse(cx[i], cy[i], size1, size1);
+        if(cy[i]> height){
+        	cy[i]=0;
+        }
+        if (cx[i]>width){
+        	cx[i]=0;
+        }
+
     }
 }
